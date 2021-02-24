36fbcd1b92cf465d0dcb69450e6a86562d8601ca
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-02-24 23:09:48 +0530

    Update

--- src/__test__/components/Assistant/MenuPreviewCard.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PreviewCard from '../../../components/Assistant/MenuPreviewCard';
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<PreviewCard />, div);
-});
